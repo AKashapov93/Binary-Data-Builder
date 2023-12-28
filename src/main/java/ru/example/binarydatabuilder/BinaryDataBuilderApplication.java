9a0ca4027067499b9d38023e8ad38d6c569f2cf9
vladimir <pruglo.vladimir@bk.ru>
2023-12-29 02:34:33 +0500

    first commit

---
+++ src/main/java/ru/example/binarydatabuilder/BinaryDataBuilderApplication.java
@@ -0,0 +1,13 @@
+package ru.example.binarydatabuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryDataBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryDataBuilderApplication.class, args);
+    }
+
+}
